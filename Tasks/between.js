58f2907ad2ec9fae7fb4189d8c6dbe1214225338
Olga Pinchuk <pinchukolya@gmail.com>
2022-01-03 15:19:59 +0100

    refactor names

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,18 +1,37 @@
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p)
-    if ( i === -1 ) return ''
-    else {
-      k = i + p.length
-      str =  str.substring(k)
-      if ( s) {
-        i = str.indexOf( s )
-        if (i === -1) {
-          return  '' } else { str =  str.substring(0 , i)
-            }}}
-  return str
-}
+// getvaluebetween = (str, p, s) => {
+//   i = str.indexOf(p)
+//     if ( i === -1 ) return ''
+//     else {
+//       k = i + p.length
+//       str =  str.substring(k)
+//       if ( s) {
+//         i = str.indexOf( s )
+//         if (i === -1) {
+//           return  '' } else { str =  str.substring(0 , i)
+//             }}}
+//   return str
+// }
 
-const result = getvaluebetween('Hello <username> and bye!', '<', '>')
-console.log(result)
+const getValueBetween = (str, prefix, suffix) => {
+  const prefixIndex = str.indexOf(prefix);
+  if (prefixIndex === -1) {
+    return "";
+  } else {
+    const startPosition = prefixIndex + prefix.length;
+    str = str.substring(startPosition);
+    if (suffix) {
+      const suffixIndex  = str.indexOf(suffix);
+      if (suffixIndex === -1) {
+        return "";
+      } else {
+        str = str.substring(0, suffixIndex);
+      }
+    }
+  }
+  return str;
+};
+
+const result = getValueBetween("Hello <username> and bye!", "<", ">");
+console.log(result);
