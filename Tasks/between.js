e3d363a87459a7137191381680c3710016151d4a
OlgaPinchuk <pinchukolya@gmail.com>
2022-02-16 12:17:55 +0100

    refactor between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,21 +1,31 @@
+'use strict';
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
-  if (i === -1) return '';
-  else {
-    k = i + p.length;
-    str = str.substring(k);
-    if (s) {
-      i = str.indexOf(s);
-      if (i === -1) {
-        return '';
-      } else {
-        str = str.substring(0, i);
-      }
-    }
-  }
-  return str;
+// getvaluebetween = (str, p, s) => {
+//   i = str.indexOf(p);
+//   if (i === -1) return '';
+//   else {
+//     k = i + p.length;
+//     str = str.substring(k);
+//     if (s) {
+//       i = str.indexOf(s);
+//       if (i === -1) {
+//         return '';
+//       } else {
+//         str = str.substring(0, i);
+//       }
+//     }
+//   }
+//   return str;
+// };
+
+
+const getValueBetween = (str, from, to) => {
+  const start = str.indexOf(from);
+  const end = to && str.indexOf(to);
+  if (start === -1 || end === -1) return '';
+  let startIndex = start + from.length;
+  return str.substring(startIndex, end);
 };
 
-require('../Tests/between.js')(getvaluebetween);
+require('../Tests/between.js')(getValueBetween);
